7d98632c13cd7d32f8ae70f56caf43a9b9daaf6b
Lenon <fernandesbruno01384@gmail.com>
2023-01-13 03:41:18 -0300

    ex008, ex009, ex009-b e ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`Your car\'s speed is ${vel}Km/h`)
+if (vel > 60){ // Simple condition!
+   console.log(`You have exceeded the permitted speed. FINED!`)
+}
+console.log(`Always drive wearing a seat belt!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var coun = 'Brasil'
+console.log(`Living in ${coun}, you are`)
+if (coun != 'Brasil'){
+   console.log('You are a foreigner!')
+} else{
+   console.log('You are Brazilian!')
+}
